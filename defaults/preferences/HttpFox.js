8fbe18f15417524eb10cb30cdaed9a2ebf8c2afa
Martin Theimer <pappkamerad@gmail.com>
2008-03-02 23:12:13 +0000

    initial checkin (v0.6.0)

---
+++ defaults/preferences/HttpFox.js
@@ -0,0 +1,4 @@
+pref("extensions.httpfox.StartAtBrowserStart", false);
+pref("extensions.httpfox.AlwaysOpenDetached", false);
+pref("extensions.httpfox.ShowHttpFoxHelperRequests", false);
+pref("extensions.httpfox.ColorRequests", false);
